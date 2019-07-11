88624f15ad30fcf25cbac669e23d01b667b1218f
Jhang512 <a10947863@hotmail.com>
2019-07-11 10:10:20 +0800

    Add read file feature

--- products.py
+++ products.py
@@ -1,7 +1,18 @@
 products = []
+# Add read file
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if 'Item,Price' in line:
+			continue
+#		s = line.strip().split(',')
+#		name = s[0]
+#		price = s[1]
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
 
+# User input start
 while True:
-
 	item = input('Please in put product\'s name: ')
 	if item == 'q':
 		break
@@ -13,9 +24,11 @@ while True:
 	products.append([item, price])
 print (products)
 
+# Print all the records
 for p in products:
 	print(p[0],'\'s price is ', p[1])
 
+# Write to a new file
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('Item,Price' + '\n')
 	for p in products:
